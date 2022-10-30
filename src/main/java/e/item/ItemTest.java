9362c7bb1d6dbdecdb5e36e33648a2860b97e0b2
euanPC <euan50emc@gmail,com>
2022-10-30 17:02:11 +0000

    e

---
+++ src/main/java/e/item/ItemTest.java
@@ -0,0 +1,113 @@
+
+package e.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import e.procedure.ProcedureTestLivingEntityIsHitWithTool;
+
+import e.ElementsCocMod;
+
+import com.google.common.collect.Multimap;
+
+@ElementsCocMod.ModElement.Tag
+public class ItemTest extends ElementsCocMod.ModElement {
+	@GameRegistry.ObjectHolder("coc:test")
+	public static final Item block = null;
+	public ItemTest(ElementsCocMod instance) {
+		super(instance, 9);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemToolCustom() {
+			@Override
+			public boolean hitEntity(ItemStack itemstack, EntityLivingBase entity, EntityLivingBase entity2) {
+				super.hitEntity(itemstack, entity, entity2);
+				int x = (int) entity.posX;
+				int y = (int) entity.posY;
+				int z = (int) entity.posZ;
+				World world = entity.world;
+				{
+					Map<String, Object> $_dependencies = new HashMap<>();
+					$_dependencies.put("entity", entity);
+					$_dependencies.put("sourceentity", entity2);
+					$_dependencies.put("x", x);
+					$_dependencies.put("y", y);
+					$_dependencies.put("z", z);
+					$_dependencies.put("world", world);
+					ProcedureTestLivingEntityIsHitWithTool.executeProcedure($_dependencies);
+				}
+				return true;
+			}
+		}.setUnlocalizedName("test").setRegistryName("test").setCreativeTab(CreativeTabs.TOOLS));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("coc:test", "inventory"));
+	}
+	private static class ItemToolCustom extends Item {
+		protected ItemToolCustom() {
+			setMaxDamage(100);
+			setMaxStackSize(1);
+		}
+
+		@Override
+		public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot) {
+			Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(equipmentSlot);
+			if (equipmentSlot == EntityEquipmentSlot.MAINHAND) {
+				multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Tool modifier", 0f, 0));
+				multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Tool modifier", -3, 0));
+			}
+			return multimap;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			IBlockState require;
+			return 0;
+		}
+
+		@Override
+		public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
+			stack.damageItem(1, entityLiving);
+			return true;
+		}
+
+		@Override
+		public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+			stack.damageItem(2, attacker);
+			return true;
+		}
+
+		@Override
+		public boolean isFull3D() {
+			return true;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 2;
+		}
+	}
+}
